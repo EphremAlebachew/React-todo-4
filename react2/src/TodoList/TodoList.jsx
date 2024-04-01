3dc952dc94bbb57d3d678973e3d1203a84a390fb
Ephrem Alebachew Tebeje <128262759+EphremAlebachew@users.noreply.github.com>
2024-04-01 03:04:42 -0500

    lifting-state-1

--- react2/src/TodoList/TodoList.jsx
+++ react2/src/TodoList/TodoList.jsx
@@ -5,7 +5,8 @@ function TodoList(props) {
 
   return (
     <div>
-      <h2>Todo List</h2>
+          <h2>Todo List</h2>
+          <h1>Todo List 1</h1>
       <ul>
         {todoList.map((todo, index) => (
           <li key={index}>{todo}</li>
